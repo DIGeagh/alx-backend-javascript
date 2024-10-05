07bba8fc1f7fdd5ef63f4d623c04dbd8dd929d89
DIGeagh <darlingtongavi@hotmail.com>
2024-10-05 01:09:14 +0000

    classes

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,21 +1,32 @@
-
 export default class HolbertonClass {
   constructor(size, location) {
-    if (typeof size === 'number') {
-      this._size = size;
-    }
-    if (typeof location === 'string') {
-      this._location = location;
-    }
+    this.size = size;
+    this.location = location;
   }
 
-  // eslint-disable-next-line consistent-return
-  [Symbol.toPrimitive](value) {
-    if (value === 'number') {
-      return this._size;
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
     }
-    if (value === 'string') {
-      return this._location;
+    if (hint === 'string') {
+      return this.location;
     }
+    return this;
   }
 }
