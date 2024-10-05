8b8e5716665b10986ff0c1da53b179e6bb7416f8
DIGeagh <darlingtongavi@hotmail.com>
2024-10-05 01:07:35 +0000

    classes

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,13 +1,7 @@
-
 export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
-  }
-
-  // Getter for Symbol.toStringTag
-  get [Symbol.toStringTag]() {
-    return this._code;
   }
 
   toString() {
